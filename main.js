cb5dbc7379659a748b075bc896662d9c021d330d
Keith Potempa <keithrpotempa@gmail.com>
2020-01-20 14:49:58 -0600

    completed Created Elements challenge, moving on to advanced challenge

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 // [x] Put an article DOM element in your index.html with the id attribute value of messages.
-// [ ] In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
+// [x] In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
 // [ ] Create five (5) section components, each with a class of message, and with the content of your choosing.
 // [ ] Using appendChild(), attach each message as a child to the messages element.
 // Example output.
 
-// <article id="messages">
-//     <section class="message">
-//         Are we doing fireworks this year?
-//     </section>
-//     <section class="message">
-//         After last year's "tree incident", should we?
-//     </section>
-//     <section class="message">
-//         The fire fighters put it out in like a minute. Wasn't even a real fire.
-//     </section>
-//     <section class="message">
-//         We can set them off in the street.
-//     </section>
-//     <section class="message">
-//         Our neighbors' houses are flammable, too
-//     </section>
-// </article>
+const messagesElement = document.querySelector("#messages");
+
+const makeMessage = (message) => {
+    return `
+        <section class="message">
+        ${message}
+        </section>
+    `
+}
+
+let messages = [
+        "Are we doing fireworks this year?",
+        `After last year's "tree incident", should we?`,
+        "The fire fighters put it out in like a minute. Wasn't even a real fire.",
+        "We can set them off in the street.",
+        "Our neighbors' houses are flammable, too",
+]
+
+for (let i = 0; i < messages.length; i++) {
+    messagesElement.innerHTML += makeMessage(messages[i]);
+}
