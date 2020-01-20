be841aeb1db86b0ba3df44f99f2ffce1d9711cdc
Keith Potempa <keithrpotempa@gmail.com>
2020-01-20 14:40:02 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// [x] Put an article DOM element in your index.html with the id attribute value of messages.
+// [ ] In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
+// [ ] Create five (5) section components, each with a class of message, and with the content of your choosing.
+// [ ] Using appendChild(), attach each message as a child to the messages element.
+// Example output.
+
+// <article id="messages">
+//     <section class="message">
+//         Are we doing fireworks this year?
+//     </section>
+//     <section class="message">
+//         After last year's "tree incident", should we?
+//     </section>
+//     <section class="message">
+//         The fire fighters put it out in like a minute. Wasn't even a real fire.
+//     </section>
+//     <section class="message">
+//         We can set them off in the street.
+//     </section>
+//     <section class="message">
+//         Our neighbors' houses are flammable, too
+//     </section>
+// </article>
