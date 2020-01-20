fe0140faf1ac18e03edc1fa7d73b71e9efc6aa5e
Keith Potempa <keithrpotempa@gmail.com>
2020-01-20 15:06:41 -0600

    completed the exercise using appendchild and createlement

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 // [x] Put an article DOM element in your index.html with the id attribute value of messages.
 // [x] In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
-// [ ] Create five (5) section components, each with a class of message, and with the content of your choosing.
-// [ ] Using appendChild(), attach each message as a child to the messages element.
-// Example output.
+// [x] Create five (5) section components, each with a class of message, and with the content of your choosing.
+// [x] Using appendChild(), attach each message as a child to the messages element.
 
 const messagesElement = document.querySelector("#messages");
-
-const makeMessage = (message) => {
-    return `
-        <section class="message">
-        ${message}
-        </section>
-    `
-}
 
 let messages = [
         "Are we doing fireworks this year?",
         `After last year's "tree incident", should we?`,
         "The fire fighters put it out in like a minute. Wasn't even a real fire.",
         "We can set them off in the street.",
-        "Our neighbors' houses are flammable, too",
+        "Our neighbors' houses are flammable, too."
 ]
 
 for (let i = 0; i < messages.length; i++) {
-    messagesElement.innerHTML += makeMessage(messages[i]);
+    const section = document.createElement("section");
+    section.innerHTML = messages[i];
+    messagesElement.appendChild(section);
 }
